0a171e2a1baabebc7381e3a30b74fef90556d213
Jiayi Zhu <yueyue200830@berkeley.edu>
2019-02-21 00:50:33 -0800

    update

--- hog_contest.py
+++ hog_contest.py
@@ -18,7 +18,7 @@ def final_strategy(score, opponent_score):
     rolls = [[0 for column in range(101)] for row in range(101)]
     num_count = [[0 for column in range(61)] for row in range(11)]
     def search(s1, s2):
-        #print("search", s1, s2)
+        # 有分数为100以上的时候
         if s1 >= 100:
             probility[100][s2] = 1
             visited[100][s2] = 1
@@ -29,33 +29,49 @@ def final_strategy(score, opponent_score):
             visited[s1][100] = 1
             rolls[100][s2] = 1
             return 0
+        # 如果有访问过
         if visited[s1][s2]:
             return probility[s1][s2]
+        # 从投0-10
         for roll in range(11):
+            # 投0的情况
             if roll == 0:
+                # 分别获取双方的投的数量，然后search获取这个情况下的概率
                 new_s1 = s1 + free_bacon(s2)
                 new_s2 = s2 + search_opponent(new_s1, s2)
                 current_pro = search(new_s1, new_s2)
+                #对比概率大小，因为pro初始是0，这个其实会覆盖的
                 if current_pro > probility[s1][s2]:
                     probility[s1][s2] = current_pro
                     rolls[s1][s2] = roll
             else:
+                #不是投0的情况， pro是概率之和
                 pro = 0
+                #投的可能值是roll-roll*6
                 for increasement in range(roll, roll*6+1):
                     new_s1 = s1 + increasement
-                    new_s2 = search_opponent(new_s1, s2)
+                    #单纯的优化，如果新的值上100，剩下投出来的值都上100，可以直接计算，概率为1
+                    if new_s1 >= 100:
+                        for j in range(increasement, roll*6+1):
+                            pro += num_count[roll][j]
+                        break
+                    #一般情况，值颗没上100，搜索对方的新的值
+                    new_s2 = s2 + search_opponent(new_s1, s2)
                     pro += search(new_s1, new_s2) *  num_count[roll][increasement]
+                #算一个总体概率
                 pro /= 6 ** roll
+                #如果概率大，覆盖
                 if pro > probility[s1][s2]:
                     probility[s1][s2] = pro
                     rolls[s1][s2] = roll
         return probility[s1][s2]
+    #用于搜索对手的值
     def search_opponent(s1, s2):
-        #print("opponent", s1, s2)
         if s1 >= 100:
             return 0
         if visited[s2][s1]:
             return rolls[s2][s1]
+        #对对方的投的值搜索
         search(s2, s1)
         return rolls[s2][s1]
     def counting_num():
@@ -67,10 +83,11 @@ def final_strategy(score, opponent_score):
                     for k in range(max(0, j-6), j):
                         num_count[i][j] += num_count[i-1][k]
     counting_num()
-    for i in range(100, -1, -1):
-        for j in range(100, -1, -1):
-            if not visited[i][j]:
-                search(i, j)
+    #从后往前遍历search
+    for i in range(200, 1, -1):
+        for j in range(max(0, i-100), min(101,i+1)):
+            if not visited[i-j][j]:
+                search(i-j, j)
     """
     for i in range(101):
         for j in range(101):
